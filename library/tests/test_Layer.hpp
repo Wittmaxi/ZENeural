b27d01c037b61f5ff0f9099292adcea3c42598c8
wittmaxi@outlook.de <wittmaxi@outlook.de>
2018-11-15 16:45:45 +0100

    refactored unit tests in test_layer.hpp

--- library/tests/test_Layer.hpp
+++ library/tests/test_Layer.hpp
@@ -6,52 +6,46 @@ https://github.com/Wittmaxi/ZENeural/blob/master/LICENSE
 
 */
 
+void testConstructor();
+void testCalculate();
+void testWeightedSum();
+void testBiasNode();
+
+void testConstructor()
+{
+    ZNN::Layer<double> layer(10, 100);
+    CHECK(layer.size == 10);
+    CHECK(layer.inputSize == 100);
+    CHECK(layer.neurons.size() == 10);
+    CHECK(layer.neurons[0].weights.size() == 100);
+}
+
+void testCalculate()
+{
+    ZNN::Layer<double> layer(1000, 100);
+    layer.normalization = ZNN::Fermi<double>();
+    std::vector<double> inputs(999, 100);
+    std::vector<double> outputs = layer.calculate(inputs);
+    CHECK(outputs.size() == 1000);
+    CHECK(outputs[0] <= 1);
+}
+
+void testWeightedSum()
+{
+    ZNN::Layer<double> layer(10, 100);
+    std::vector<double> inputs(9, 100);
+    CHECK(layer.weightedSum(inputs).size() == 10);
+}
+
+void testBiasNode()
+{
+    ZNN::Layer<double> layer(10, 11);
+    std::vector<double> inputs(10, 0);
+    CHECK(layer.calculate(inputs)[0] != 0.5);
+}
+
 TEST_CASE("Layer")
 {
-    struct testConstructor : public ZNN::Layer<double>
-    {
-        testConstructor()
-        {
-            Layer<double> layer(10, 100);
-            CHECK(layer.size == 10);
-            CHECK(layer.inputSize == 100);
-            CHECK(layer.neurons.size() == 10);
-            CHECK(layer.neurons[0].weights.size() == 100);
-        }
-    };
-
-    struct testCalculate : public ZNN::Layer<double>
-    {
-        testCalculate()
-        {
-            Layer<double> layer(1000, 100);
-            layer.normalization = ZNN::Fermi<double>();
-            std::vector<double> inputs(999, 100);
-            std::vector<double> outputs = layer.calculate(inputs);
-            CHECK(outputs.size() == 1000);
-            CHECK(outputs[0] <= 1);
-        }
-    };
-
-    struct testWeightedSum : public ZNN::Layer<double>
-    {
-        testWeightedSum()
-        {
-            Layer<double> layer(10, 100);
-            std::vector<double> inputs(9, 100);
-            CHECK(layer.weightedSum(inputs).size() == 10);
-        }
-    };
-
-    struct testBiasNode : public ZNN::Layer<double>
-    {
-        testBiasNode()
-        {
-            Layer<double> layer(10, 11);
-            std::vector<double> inputs(10, 0);
-            CHECK(layer.calculate(inputs)[0] != 0.5);
-        }
-    };
 
     testConstructor();
     testCalculate();
