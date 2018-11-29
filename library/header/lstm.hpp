52182959bc80c2db50cb5a626ac3c47dbf7c8e31
wittmaxi@outlook.de <wittmaxi@outlook.de>
2018-11-29 21:37:00 +0100

    added LSTM

---
+++ library/header/lstm.hpp
@@ -0,0 +1,17 @@
+/*
+2018 Maximilian Wittmer
+wittmaxi@linux.pizza
+
+https://github.com/Wittmaxi/ZENeural/blob/master/LICENSE
+
+*/
+
+#pragma once
+#include "VanillaRecurrentNetwork.hpp"
+
+namespace ZNN {
+template<class floatType>
+class VanillaRecurrentNeuralNetwork {
+
+}
+}
